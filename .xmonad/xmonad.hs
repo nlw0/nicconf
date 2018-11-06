f0f146c1dbe79490f6a8a2800a406b2604181a23
nic <nicolau.lealwerneck@tomtom.com>
2018-11-06 09:53:31 +0100

    dvorak compatible bindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Fullscreen
+import XMonad.Layout.SimpleFloat
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig
 -- import XMonad.Hooks.EwmhDesktops
@@ -24,14 +25,13 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
- 
 
 baseConfig = desktopConfig
 
 myConfig = baseConfig
-	{ terminal = "xterm"
-	, modMask = mod4Mask
-	, layoutHook = myLayout
+	   { terminal = "xterm"
+	   , modMask = mod4Mask
+	   , layoutHook = myLayout
 	, borderWidth = 1
 	, manageHook = myManageHook
         , handleEventHook = mconcat [ docksEventHook, E.fullscreenEventHook ]
@@ -41,6 +41,7 @@ myConfig = baseConfig
 myLayout = avoidStruts (
 	Tall 1 (3/100) (1/2) |||
 	Mirror (Tall 1 (3/100) (1/2))) |||
+	simpleFloat |||
 	noBorders (fullscreenFull Full)
 
 myManageHook = composeAll
@@ -62,7 +63,19 @@ myKeys = [
 	, ((mod4Mask              , xK_z     ), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
 	, ((mod4Mask              , xK_o     ), spawn "xscreensaver-command -lock")
 	]
-	
+
+envSwitchKeys = [
+	(otherModMasks ++ "M-" ++ [key], action tag)
+	| (tag, key)  <- zip myWorkspaces "123qweasd"
+	, (otherModMasks, action) <- [ ("", windows . W.greedyView) -- was W.greedyView
+                                     , ("S-", windows . W.shift)]
+	] ++ [
+	(otherModMasks ++ "M-" ++ [key], action tag)
+	| (tag, key)  <- zip ["4","5","9","8"] "/,.o"
+	, (otherModMasks, action) <- [ ("", windows . W.greedyView) -- was W.greedyView
+                                     , ("S-", windows . W.shift)]
+	] -- for dvorak mode, flips 9 and 6 relative to qwerty
+
 myKeysP = ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
         : ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
         : ("<XF86AudioLowerVolume>", spawn "amixer -D pulse sset Master 5%-")
@@ -70,12 +83,10 @@ myKeysP = ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
         : ("<XF86AudioMute>", spawn "amixer -D pulse sset Master toggle")
         : ("M-<F12>", spawn "scrot -e 'xclip -target image/png -selection c -i $f'")
         : ("M-S-<F12>", spawn "sleep 0.5; scrot -d 1 -s -e 'xclip -target image/png -selection c -i $f'")
+        : ("M-[", spawn "xterm -e \"ssh -Y nic@10.71.2.15\"")
         : ("M-]", spawn "xterm -e \"ssh -Y nic@10.71.2.14\"")
+        : ("M-S-[", spawn "xrandr --output DP-1-1-1 --rotate left")
+        : ("M-S-]", spawn "xrandr --output DP-1-1-1 --rotate left; xrandr --output DP-1-1-1 --rotate normal")
         : ("M-<Return>", spawn "emacsclient -c")
         : ("M-0", spawn "keyboard_layout_change.sh")
-        : [
-	(otherModMasks ++ "M-" ++ [key], action tag)
-	| (tag, key)  <- zip myWorkspaces "123qweasd"
-	, (otherModMasks, action) <- [ ("", windows . W.greedyView) -- was W.greedyView
-                                     , ("S-", windows . W.shift)]
-	]
+        : envSwitchKeys
